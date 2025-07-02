d9f1da49380bf35dcf9aeed3ae97b6730072cd8b
David Campbell <dcampbell24@gmail.com>
2025-07-02 15:00:37 -0400

    Further futhorc.

--- src/bin/futhorc.rs
+++ src/bin/futhorc.rs
@@ -6,48 +6,52 @@ fn main() -> Result<(), anyhow::Error> {
         let mut line = String::new();
         io::stdin().read_line(&mut line)?;
 
-        let output_1 = translate_to_runic_2(&line);
+        line = translate_to_runic_2(&line);
+        line = translate_to_runic(&line);
 
-        let mut output_2 = String::new();
-        for char in output_1.chars() {
-            output_2.push_str(&translate_to_runic(char));
-        }
-
-        println!("{output_2}");
+        println!("{line}");
     }
 }
 
-fn translate_to_runic(c: char) -> String {
-    match c {
-        'ɑ' => 'ᚪ'.to_string(),             // f_a_r
-        'ɔ' => 'ᛟ'.to_string(),             // h_o_t
-        'æ' => 'ᚫ'.to_string(),             // h_a_t
-        'ɛ' => 'ᛖ'.to_string(),             // s_e_nd
-        'ɪ' => 'ᛁ'.to_string(),             // s_i_t
-        'i' => "ᛁᛁ".to_string(),            // s_ee_d
-        'ʊ' | 'u' => 'ᚣ'.to_string(),       // b_oo_k, f_oo_d
-        'ə' | 'ʌ' | 'ɜ' => 'ᚢ'.to_string(), // _a_bout, f_u_n, t_u_rn
-        'p' => 'ᛈ'.to_string(),             // _p_ot
-        'b' => 'ᛒ'.to_string(),             // _b_oy
-        't' => 'ᛏ'.to_string(),             // _t_ime
-        'd' => 'ᛞ'.to_string(),             // _d_og
-        'k' => 'ᚳ'.to_string(),             // _k_ite
-        'g' => 'ᚷ'.to_string(),             // _g_ame
-        'f' | 'v' => 'ᚠ'.to_string(),       // _f_ear, _v_ine
-        'θ' | 'ð' => 'ᚦ'.to_string(),       // _th_ing, _th_is
-        's' => "ᛋᛋ".to_string(),            // _s_ee, lot_s_
-        'z' => 'ᛋ'.to_string(),             // _z_ebra, song_s_
-        'ʃ' | 'ʒ' => "ᛋᚻ".to_string(),      // _sh_are, mea_s_ure
-        'h' => 'ᚻ'.to_string(),             // _h_ole
-        'm' => 'ᛗ'.to_string(),             // _m_outh
-        'n' => 'ᚾ'.to_string(),             // _n_ow
-        'ŋ' => 'ᛝ'.to_string(),             // ri_ng_
-        'j' => 'ᛄ'.to_string(),             // _y_ou
-        'w' => 'ᚹ'.to_string(),             // _w_ind
-        'ɹ' => 'ᚱ'.to_string(),             // _r_ain
-        'l' => 'ᛚ'.to_string(),             // _l_ine
-        c => c.to_string(),
+fn translate_to_runic(string: &str) -> String {
+    let mut output = String::new();
+
+    for char in string.chars() {
+        let runes = match char {
+            'ɑ' => "ᚪ",             // f_a_r
+            'ɔ' => "ᛟ",             // h_o_t
+            'æ' => "ᚫ",             // h_a_t
+            'ɛ' => "ᛖ",             // s_e_nd
+            'ɪ' => "ᛁ",             // s_i_t
+            'i' => "ᛁᛁ",            // s_ee_d
+            'ʊ' | 'u' => "ᚣ",       // b_oo_k, f_oo_d
+            'ə' | 'ʌ' | 'ɜ' => "ᚢ", // _a_bout, f_u_n, t_u_rn
+            'p' => "ᛈ",             // _p_ot
+            'b' => "ᛒ",             // _b_oy
+            't' => "ᛏ",             // _t_ime
+            'd' => "ᛞ",             // _d_og
+            'k' => "ᚳ",             // _k_ite
+            'g' => "ᚷ",             // _g_ame
+            'f' | 'v' => "ᚠ",       // _f_ear, _v_ine
+            'θ' | 'ð' => "ᚦ",       // _th_ing, _th_is
+            's' => "ᛋᛋ",            // _s_ee, lot_s_
+            'z' => "ᛋ",             // _z_ebra, song_s_
+            'ʃ' | 'ʒ' => "ᛋᚻ",      // _sh_are, mea_s_ure
+            'h' => "ᚻ",             // _h_ole
+            'm' => "ᛗ",             // _m_outh
+            'n' => "ᚾ",             // _n_ow
+            'ŋ' => "ᛝ",             // ri_ng_
+            'j' => "ᛄ",             // _y_ou
+            'w' => "ᚹ",             // _w_ind
+            'ɹ' => "ᚱ",             // _r_ain
+            'l' => "ᛚ",             // _l_ine
+            c => &c.to_string(),
+        };
+
+        output.push_str(runes);
     }
+
+    output
 }
 
 fn translate_to_runic_2(string: &str) -> String {
