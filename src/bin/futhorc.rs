2aa6d5ea897e8e0be1b918f0c7388304efbf3b0e
David Campbell <dcampbell24@gmail.com>
2025-07-03 19:37:29 -0400

    Add a note to translate.

--- src/bin/futhorc.rs
+++ src/bin/futhorc.rs
@@ -6,6 +6,7 @@ use std::{
     fs::{self, File},
     io,
     path::PathBuf,
+    println,
 };
 
 #[cfg(feature = "zip")]
@@ -72,6 +73,26 @@ fn words_hash() -> Result<HashMap<String, String>, anyhow::Error> {
     Ok(words_hash)
 }
 
+// 1.    Apostrophes and punctuation are used just like in standard English.
+// 2.    /i/ at the end of a word or before an apostrophe is simply ᛁ. So you
+//       have we'll/ᚹᛁ'ᛚ, will/ᚹᛁᛚ, wheel/ᚹᛁᛁᛚ. This applies to morphemes too:
+//       you have any/ᛖᚾᛁ, anything/ᛖᚾᛁᚦᛁᛝ. Also note that ᛁᛁ may represent /iɪ/ as in being/ᛒᛁᛁᛝ.
+// 3.    If there is ambiguity between /f/ and /v/, use ᚠᚠ for /f/ and ᚠ for /v/.
+//       So you have live/ᛚᛁᚠ, leave/ᛚᛁᛁᚠ, leaf/ᛚᛁᛁᚠᚠ, lives/ᛚᛁᚠᛋ, leaves/ᛚᛁᛁᚠᛋ.
+//       Note that rules apply in the order they are listed here in case of a conflict.
+// 4.    There is similar ambiguity clarification as above for /s/ (ᛋᛋ) and /z/
+//       (ᛋ), So you have ones/ᚹᚢᚾᛋ, once/ᚹᚢᚾᛋᛋ
+// 5.  X "No" is spelled ᚾᚩ and "know" is spelled ᚾᚩᚹ.
+// 6.    Words which use "tr" for /tʃɹ/ in standard English are spelled with ᛏᚱ,
+//       not ᚳᚻᚱ. Similar for "dr"/ᛞᚱ and /dʒɹ/; "x"/ᛉ and /ks/. So you have
+//       truck/ᛏᚱᚢᚳ, draw/ᛞᚱᛟ, tax/ᛏᚫᛉ, racks/ᚱᚫᚳᛋ.
+// 7.    Word-final /ə/ is written ᚪ. So you have comma/ᚳᛟᛗᚪ (not ᚳᛟᛗᚢ),
+//       vanilla/ᚠᚢᚾᛁᛚᚪ. Exception: the/ᚦᛖ.
+// 8.    Syllabic consonants are spelled with ᚢ before the consonant. So you have bottle/ᛒᛟᛏᚢᛚ.
+// 9.    ᛋ and ᛏ are optionally written together as the ligature ᛥ as in stone/ᛥᚩᚾ.
+//       Likewise for ᚳᚹ becoming ᛢ.
+// 10.   The name of this alphabet is written ᚠᚢᚦᚩᚱᚳ, but pronounced /fuθork/
+//       like "FOO-thork" as if it were spelled ᚠᚣᚦᚩᚱᚳ.
 #[must_use]
 fn translate(mut line: String, words: &HashMap<String, String>) -> String {
     line.make_ascii_lowercase();
