78b349699c377f2623d93fa95f35f6b8bf446e7e
David Campbell <dcampbell24@gmail.com>
2025-07-04 17:26:38 -0400

    Document words_to_runes.

--- src/futhorc.rs
+++ src/futhorc.rs
@@ -1,27 +1,67 @@
 //! Futhorc created by Harys Dalvi (<https://www.harysdalvi.com/futhorc/>)
 
-// 1.    Apostrophes and punctuation are used just like in standard English.
-// 2.    /i/ at the end of a word or before an apostrophe is simply ᛁ. So you
-//       have we'll/ᚹᛁ'ᛚ, will/ᚹᛁᛚ, wheel/ᚹᛁᛁᛚ. This applies to morphemes too:
-//       you have any/ᛖᚾᛁ, anything/ᛖᚾᛁᚦᛁᛝ. Also note that ᛁᛁ may represent /iɪ/ as in being/ᛒᛁᛁᛝ.
-// 3.  X If there is ambiguity between /f/ and /v/, use ᚠᚠ for /f/ and ᚠ for /v/.
-//       So you have live/ᛚᛁᚠ, leave/ᛚᛁᛁᚠ, leaf/ᛚᛁᛁᚠᚠ, lives/ᛚᛁᚠᛋ, leaves/ᛚᛁᛁᚠᛋ.
-//       Note that rules apply in the order they are listed here in case of a conflict.
-// 4.  X There is similar ambiguity clarification as above for /s/ (ᛋᛋ) and /z/
-//       (ᛋ), So you have ones/ᚹᚢᚾᛋ, once/ᚹᚢᚾᛋᛋ
-// 5.  X "No" is spelled ᚾᚩ and "know" is spelled ᚾᚩᚹ.
-// 6.    Words which use "tr" for /tʃɹ/ in standard English are spelled with ᛏᚱ,
-//       not ᚳᚻᚱ. Similar for "dr"/ᛞᚱ and /dʒɹ/; "x"/ᛉ and /ks/. So you have
-//       truck/ᛏᚱᚢᚳ, draw/ᛞᚱᛟ, tax/ᛏᚫᛉ, racks/ᚱᚫᚳᛋ.
-// 7.    Word-final /ə/ is written ᚪ. So you have comma/ᚳᛟᛗᚪ (not ᚳᛟᛗᚢ),
-//       vanilla/ᚠᚢᚾᛁᛚᚪ. Exception: the/ᚦᛖ.
-// 8.    Syllabic consonants are spelled with ᚢ before the consonant. So you have bottle/ᛒᛟᛏᚢᛚ.
-// 9.    ᛋ and ᛏ are optionally written together as the ligature ᛥ as in stone/ᛥᚩᚾ.
-//       Likewise for ᚳᚹ becoming ᛢ.
-// 10.   The name of this alphabet is written ᚠᚢᚦᚩᚱᚳ, but pronounced /fuθork/
-//       like "FOO-thork" as if it were spelled ᚠᚣᚦᚩᚱᚳ.
-
 use std::collections::HashMap;
+
+/// # Javascript Package
+///
+/// In order to run the javascript pkg:
+///
+/// ```sh
+/// cargo install wasm-pack
+/// make js
+/// ```
+///
+/// Then copy the pkg folder to your web browser's site folder. For example with Apache on Debian:
+///
+/// ```sh
+/// sudo mkdir --parent /var/www/html/pkg
+/// sudo cp -r pkg /var/www/html
+/// ```
+///
+/// Or if you installed the package via npm:
+///
+/// ```sh
+/// sudo mkdir --parent /var/www/html/pkg
+/// sudo cp ~/node_modules/hnefatafl-copenhagen/* /var/www/html/pkg
+/// ```
+///
+/// Then load the javascript on a webpage:
+///
+/// ```sh
+/// cat << EOF > /var/www/html/index.html
+/// <!DOCTYPE html>
+/// <html>
+/// <head>
+///     <title>Copenhagen Hnefatafl</title>
+/// </head>
+/// <body>
+///     <h1>Copenhagen Hnefatafl</h1>
+///     <script type="module">
+///         import init, { Ipa, ipa_to_runes_js } from '../pkg/hnefatafl_copenhagen.js';
+///
+///         init().then(() => {
+///             const word = "know";
+///             console.log(word);
+///
+///             const ipa = new Ipa();
+///             const ipa_word = ipa.translate_js(word)
+///             console.log(ipa_word);
+///
+///             const rune_word = ipa_to_runes_js(ipa_word);
+///             console.log(rune_word);
+///         });
+///     </script>
+/// </body>
+/// </html>
+/// EOF
+/// ```
+#[must_use]
+pub fn words_to_runes(words: String) -> String {
+    let ipa = Ipa::default();
+
+    let ipa_words = ipa.translate(words);
+    ipa_to_runes(&ipa_words)
+}
 
 #[cfg(feature = "js")]
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -67,19 +107,40 @@ impl Ipa {
     }
 
     #[must_use]
-    pub fn translate_js(&self, line: String) -> String {
-        self.translate(line)
+    pub fn translate_js(&self, words: String) -> String {
+        self.translate(words)
     }
 }
 
 impl Ipa {
+    // 1.    Apostrophes and punctuation are used just like in standard English.
+    // 2.    /i/ at the end of a word or before an apostrophe is simply ᛁ. So you
+    //       have we'll/ᚹᛁ'ᛚ, will/ᚹᛁᛚ, wheel/ᚹᛁᛁᛚ. This applies to morphemes too:
+    //       you have any/ᛖᚾᛁ, anything/ᛖᚾᛁᚦᛁᛝ. Also note that ᛁᛁ may represent /iɪ/ as in being/ᛒᛁᛁᛝ.
+    // 3.  X If there is ambiguity between /f/ and /v/, use ᚠᚠ for /f/ and ᚠ for /v/.
+    //       So you have live/ᛚᛁᚠ, leave/ᛚᛁᛁᚠ, leaf/ᛚᛁᛁᚠᚠ, lives/ᛚᛁᚠᛋ, leaves/ᛚᛁᛁᚠᛋ.
+    //       Note that rules apply in the order they are listed here in case of a conflict.
+    // 4.  X There is similar ambiguity clarification as above for /s/ (ᛋᛋ) and /z/
+    //       (ᛋ), So you have ones/ᚹᚢᚾᛋ, once/ᚹᚢᚾᛋᛋ
+    // 5.  X "No" is spelled ᚾᚩ and "know" is spelled ᚾᚩᚹ.
+    // 6.    Words which use "tr" for /tʃɹ/ in standard English are spelled with ᛏᚱ,
+    //       not ᚳᚻᚱ. Similar for "dr"/ᛞᚱ and /dʒɹ/; "x"/ᛉ and /ks/. So you have
+    //       truck/ᛏᚱᚢᚳ, draw/ᛞᚱᛟ, tax/ᛏᚫᛉ, racks/ᚱᚫᚳᛋ.
+    // 7.    Word-final /ə/ is written ᚪ. So you have comma/ᚳᛟᛗᚪ (not ᚳᛟᛗᚢ),
+    //       vanilla/ᚠᚢᚾᛁᛚᚪ. Exception: the/ᚦᛖ.
+    // 8.    Syllabic consonants are spelled with ᚢ before the consonant. So you have bottle/ᛒᛟᛏᚢᛚ.
+    // 9.    ᛋ and ᛏ are optionally written together as the ligature ᛥ as in stone/ᛥᚩᚾ.
+    //       Likewise for ᚳᚹ becoming ᛢ.
+    // 10.   The name of this alphabet is written ᚠᚢᚦᚩᚱᚳ, but pronounced /fuθork/
+    //       like "FOO-thork" as if it were spelled ᚠᚣᚦᚩᚱᚳ.
     #[allow(clippy::missing_panics_doc)]
     #[must_use]
-    pub fn translate(&self, mut line: String) -> String {
-        line.make_ascii_lowercase();
+    pub fn translate(&self, mut words: String) -> String {
+        words.make_ascii_lowercase();
         let mut ipa_words = Vec::new();
 
-        for word in line.split_whitespace() {
+        // Fixme: all whitespace is turned into a space.
+        for word in words.split_whitespace() {
             let mut word = word.to_string();
             let mut ch = ' ';
             if word.ends_with('.') || word.ends_with('!') || word.ends_with('?') {
@@ -252,15 +313,15 @@ mod tests {
     fn know_no_etc() {
         let ipa = Ipa::default();
 
-        let mut line = String::new();
-        line.push_str("no\n");
-        let mut output = ipa.translate(line);
+        let mut words = String::new();
+        words.push_str("no");
+        let mut output = ipa.translate(words);
         output = ipa_to_runes(&output);
         assert_eq!(output, "ᚾᚩ");
 
-        let mut line = String::new();
-        line.push_str("know\n");
-        let mut output = ipa.translate(line);
+        let mut words = String::new();
+        words.push_str("know");
+        let mut output = ipa.translate(words);
         output = ipa_to_runes(&output);
         assert_eq!(output, "ᚾᚩᚹ");
     }
