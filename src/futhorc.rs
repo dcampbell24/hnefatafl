b908e76bce6ba0d1d073cef04e87e1442b149c19
David Campbell <dcampbell24@gmail.com>
2025-07-06 18:54:34 -0400

    Update todos.

--- src/futhorc.rs
+++ src/futhorc.rs
@@ -124,11 +124,11 @@ impl Ipa {
     // 6.    Words which use "tr" for /tʃɹ/ in standard English are spelled with ᛏᚱ,
     //       not ᚳᚻᚱ. Similar for "dr"/ᛞᚱ and /dʒɹ/; "x"/ᛉ and /ks/. So you have
     //       truck/ᛏᚱᚢᚳ, draw/ᛞᚱᛟ, tax/ᛏᚫᛉ, racks/ᚱᚫᚳᛋ.
-    // 7.  X Word-final /ə/ (Added or ʌ or ɜ)is written ᚪ. So you have comma/ᚳᛟᛗᚪ (not ᚳᛟᛗᚢ),
+    // 7.  X Word-final /ə/ (Added or ʌ or ɜ) is written ᚪ. So you have comma/ᚳᛟᛗᚪ (not ᚳᛟᛗᚢ),
     //       vanilla/ᚠᚢᚾᛁᛚᚪ. Exception: the/ᚦᛖ.
     // 8.    Syllabic consonants are spelled with ᚢ before the consonant. So you have bottle/ᛒᛟᛏᚢᛚ.
-    // 9.    ᛋ and ᛏ are optionally written together as the ligature ᛥ as in stone/ᛥᚩᚾ.
-    //       Likewise for ᚳᚹ becoming ᛢ.
+    // 9.  X ᛋ and ᛏ are optionally written together as the ligature ᛥ as in stone/ᛥᚩᚾ.
+    //       Likewise for ᚳᚹ becoming ᛢ (Optional, not doing...).
     // 10. X The name of this alphabet is written ᚠᚢᚦᚩᚱᚳ, but pronounced /fuθork/
     //       like "FOO-thork" as if it were spelled ᚠᚣᚦᚩᚱᚳ.
     #[allow(clippy::missing_panics_doc)]
