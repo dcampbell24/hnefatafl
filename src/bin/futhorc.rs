0f50c40ed2db145d287e2db034fa535d97540b4f
David Campbell <dcampbell24@gmail.com>
2025-07-06 23:02:10 -0400

    Allow invalid words.

--- src/bin/futhorc.rs
+++ src/bin/futhorc.rs
@@ -2,7 +2,7 @@
 
 use std::io;
 
-use hnefatafl_copenhagen::futhorc::{Ipa, ipa_to_runes};
+use hnefatafl_copenhagen::futhorc::Ipa;
 
 fn main() -> Result<(), anyhow::Error> {
     let ipa = Ipa::default();
@@ -11,8 +11,6 @@ fn main() -> Result<(), anyhow::Error> {
         let mut line = String::new();
         io::stdin().read_line(&mut line)?;
 
-        let ipa_words = ipa.translate(line);
-        print!("{ipa_words}");
-        print!("{}", ipa_to_runes(&ipa_words));
+        print!("{}", ipa.translate(line));
     }
 }
