e4da8f149ae2a8e60362c39c9ee7d321fe2a082a
David Campbell <dcampbell24@gmail.com>
2025-07-12 22:41:18 -0400

    Update the website.

--- website/website.sh
+++ website/website.sh
@@ -2,9 +2,9 @@
 
 mdbook build --dest-dir /var/www/html/
 
-sed -i 's/<meta name="description" content=".*">/<meta name="description" content="Determine how to install the software. Install using the Arch User Repository, https:\/\/community.chocolatey.org, a .deb package, https:\/\/flathub.org, or cargo.">/' /var/www/html/install.html
-sed -i 's/<meta name="description" content=".*">/<meta name="description" content="Learn the rules to the game of Copenhagen Hnefatafl. Move your pieces until you achieve victory or lose. Try not to get surrounded as the defenders and escape.">/' /var/www/html/rules.html
-sed -i '/<!-- Custom HTML head -->/a \        <link rel="canonical" href="https:\/\/hnefatafl.org" \/>' /var/www/html/index.html
+sed --in-place 's/<meta name="description" content=".*">/<meta name="description" content="Determine how to install Copenhagen Hnefatafl. Install using the Arch User Repository, Chocolatey, a Debian package (.deb), a flathub package, or Rust's cargo.">/' /var/www/html/install.html
+sed --in-place 's/<meta name="description" content=".*">/<meta name="description" content="Learn the rules to the game of Copenhagen Hnefatafl. Move your pieces until you achieve victory or lose. Try not to get surrounded as the defenders and escape.">/' /var/www/html/rules.html
+sed --in-place '/<!-- Custom HTML head -->/a \        <link rel="canonical" href="https:\/\/hnefatafl.org" \/>' /var/www/html/index.html
 
 cat << EOF > /var/www/html/robots.txt
 User-agent: *
