48c3a99e56987261ca59d913d35479712c3e4f12
David Campbell <dcampbell24@gmail.com>
2025-09-30 14:43:55 -0400

    Add monte_carlo to benchmarks.

--- benches/hnefatafl_rs.rs
+++ benches/hnefatafl_rs.rs
@@ -2,16 +2,25 @@ use std::time::Duration;
 
 use criterion::{Criterion, criterion_group, criterion_main};
 
-use hnefatafl_copenhagen::hnefatafl_rs;
+use hnefatafl_copenhagen::{board::BoardSize, game_tree::Tree, hnefatafl_rs};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("hnefatafl_rs", |b| b.iter(hnefatafl_rs));
+fn game_play_outs(c: &mut Criterion) {
+    c.bench_function("game_play_outs", move |b| b.iter(hnefatafl_rs));
+}
+
+fn monte_carlo(c: &mut Criterion) {
+    c.bench_function("monte_carlo", move |b| {
+        b.iter(|| {
+            let mut tree = Tree::new(BoardSize::_11);
+            let _plays = tree.monte_carlo_tree_search(10);
+        });
+    });
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().measurement_time(Duration::from_secs(25));
-    targets = criterion_benchmark
+    targets = game_play_outs, monte_carlo
 }
 
 criterion_main!(benches);
