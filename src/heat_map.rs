c35ae9654e9394e30651d6085ef902e55b210121
David Campbell <dcampbell24@gmail.com>
2025-10-10 16:33:05 -0400

    Make progress on the client monte carlo.

--- src/heat_map.rs
+++ src/heat_map.rs
@@ -1,13 +1,82 @@
 use std::{collections::HashMap, fmt};
 
 use crate::{
+    board::BoardSize,
     game_tree::Node,
     play::{Plae, Vertex},
     role::Role,
 };
 
 #[derive(Clone, Debug, Default)]
-pub struct HeatMap(HashMap<(Role, Vertex), Vec<f64>>);
+pub struct HeatMap {
+    pub board_size: BoardSize,
+    pub spaces: HashMap<(Role, Vertex), Vec<f64>>,
+}
+
+impl HeatMap {
+    #[allow(clippy::missing_panics_doc)]
+    #[must_use]
+    pub fn draw(&self, role: Role) -> Vec<f64> {
+        let board_size: usize = self.board_size.into();
+
+        let mut spaces = if self.board_size == BoardSize::_11 {
+            vec![0.0; 11 * 11]
+        } else {
+            vec![0.0; 13 * 13]
+        };
+
+        let mut froms = Vec::new();
+        for key in self.spaces.keys() {
+            let mut vector = self.spaces[key].clone();
+
+            for i in &mut vector {
+                if *i != 0.0 {
+                    *i = f64::midpoint(*i, 1.0);
+                }
+            }
+
+            let max = vector
+                .iter()
+                .max_by(|a, b| f64::total_cmp(a, b))
+                .expect("there is at least one value");
+
+            froms.push((key, *max));
+        }
+
+        froms.sort_by(|a, b| f64::total_cmp(&a.1, &b.1));
+        let mut froms_hash_map = HashMap::new();
+        let mut set_score = 1.0;
+        for (play, score) in &mut froms {
+            *score = set_score;
+            set_score -= 0.1;
+            froms_hash_map.insert(*play, *score);
+        }
+
+        // Fixme: fix the defender!
+        // This is empty for the defender!
+        // println!("{froms:?}");
+        // Handle clicking on the board.
+
+        for y in 0..board_size {
+            for x in 0..board_size {
+                if let Some(i) = froms_hash_map.get(&(
+                    role,
+                    Vertex {
+                        x,
+                        y,
+                        size: self.board_size,
+                    },
+                )) {
+                    spaces[y * board_size + x] = *i;
+                } else {
+                    spaces[y * board_size + x] = 0.0;
+                }
+            }
+        }
+
+        spaces
+    }
+}
 
 impl From<&Vec<&Node>> for HeatMap {
     #[allow(clippy::float_cmp)]
@@ -21,8 +90,10 @@ impl From<&Vec<&Node>> for HeatMap {
                     Plae::Play(play) => {
                         let board_index: usize = (&play.to).into();
 
+                        // Fixme!
+                        println!("{}", play.role);
                         heat_map
-                            .0
+                            .spaces
                             .entry((play.role, play.from.clone()))
                             .and_modify(|board| {
                                 let score = board
@@ -55,9 +126,13 @@ impl From<&Vec<&Node>> for HeatMap {
 
 impl fmt::Display for HeatMap {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for ((role, vertex), board) in &self.0 {
-            let board_size = if board.len() == 11 * 11 { 11 } else { 13 };
+        let board_size = if self.board_size == BoardSize::_11 {
+            11
+        } else {
+            13
+        };
 
+        for ((role, vertex), board) in &self.spaces {
             writeln!(f, "vertex: {vertex}, role: {role}")?;
             writeln!(
                 f,
