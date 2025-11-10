81a51a5fa7b7042224c9af02bb7bbdea53698dbb
David Campbell <dcampbell24@gmail.com>
2025-11-10 02:17:51 -0500

    WIP Nix.

--- packages/Nix/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-{
-  hnefatafl-copenhagen = pkgs.callPackage ./hnefatafl-copenhagen.nix { };
-}
