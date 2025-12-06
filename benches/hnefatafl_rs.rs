333a7a7dd9d5d8aa6ab9fe8e620057eb1ef15d68
David Campbell <dcampbell24@gmail.com>
2025-12-06 00:56:35 -0500

    Make criterion an optional dependency under bench.

--- benches/hnefatafl_rs.rs
+++ benches/hnefatafl_rs.rs
@@ -1,9 +1,13 @@
+#[cfg(feature = "bench")]
 use std::time::Duration;
 
+#[cfg(feature = "bench")]
 use criterion::{Criterion, criterion_group, criterion_main};
 
+#[cfg(feature = "bench")]
 use hnefatafl_copenhagen::{hnefatafl_rs, setup_hnefatafl_rs};
 
+#[cfg(feature = "bench")]
 fn game_play_outs(c: &mut Criterion) {
     let game_records = setup_hnefatafl_rs().unwrap();
     c.bench_function("game_play_outs", move |b| {
@@ -11,10 +15,17 @@ fn game_play_outs(c: &mut Criterion) {
     });
 }
 
+#[cfg(feature = "bench")]
 criterion_group! {
     name = benches;
     config = Criterion::default().measurement_time(Duration::from_secs(10));
     targets = game_play_outs
 }
 
+#[cfg(feature = "bench")]
 criterion_main!(benches);
+
+#[cfg(not(feature = "bench"))]
+fn main() {
+    eprintln!("You must enable pass `--features=bench`");
+}
