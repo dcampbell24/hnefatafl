0f1f6a59dd703718378f2252d145c6eb36758a50
David Campbell <dcampbell24@gmail.com>
2025-12-20 17:07:57 -0500

    Bump the apt revision.

--- packages/debian/generate-release.sh
+++ packages/debian/generate-release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-PACKAGE='hnefatafl-copenhagen_4.5.0-1_amd64.deb'
+PACKAGE='hnefatafl-copenhagen_4.5.0-2_amd64.deb'
 
 packages/debian/deb.sh
 
@@ -20,7 +20,7 @@ Origin: Hnefatafl Org
 Label: Hnefatafl Copenhagen
 Suite: stable
 Codename: stable
-Version: 4.5.0-1
+Version: 4.5.0-2
 Architectures: amd64
 Components: main
 Description: A software repository containing Hnefatafl Copenhagen
