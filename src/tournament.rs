9a0b263ea3e9015f2499da03da67d2354de2d129
David Campbell <dcampbell24@gmail.com>
2025-12-27 01:00:29 -0500

    Complete more of tournament.

--- src/tournament.rs
+++ src/tournament.rs
@@ -2,12 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Tournament {
-    pub tree: Vec<Option<String>>,
-}
-
-impl Tournament {
-    #[must_use]
-    pub fn new(vec: Vec<Option<String>>) -> Self {
-        Tournament { tree: vec }
-    }
+    pub byes: Vec<String>,
+    pub round_one: Vec<String>,
 }
