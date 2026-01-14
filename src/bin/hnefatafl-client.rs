5ddce1cf3e99bc156dba19f7dd7fa3199062348b
David Campbell <dcampbell24@gmail.com>
2026-01-13 19:49:15 -0500

    I don't think I'll ever deny all that...

--- src/bin/hnefatafl-client.rs
+++ src/bin/hnefatafl-client.rs
@@ -13,13 +13,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// #![deny(clippy::expect_used)]
-// #![deny(clippy::indexing_slicing)]
-#![deny(clippy::panic)]
-// #![deny(clippy::unwrap_used)]
-
 // Don't open the terminal on Windows.
 #![cfg_attr(all(windows, not(feature = "console")), windows_subsystem = "windows")]
+#![deny(clippy::panic)]
 
 use std::{
     collections::{HashMap, HashSet, VecDeque},
