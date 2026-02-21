fec42a72ec9b49adcfe63c2476d0533fa30edc89
David Campbell <noreply@hnefatafl.org>
2026-02-21 17:29:31 -0500

    Make progress...

--- src/bin/hnefatafl-server-full/main.rs
+++ src/bin/hnefatafl-server-full/main.rs
@@ -14,6 +14,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #![deny(clippy::indexing_slicing)]
+#![deny(clippy::expect_used)]
 #![deny(clippy::panic)]
 #![deny(clippy::unwrap_used)]
 
