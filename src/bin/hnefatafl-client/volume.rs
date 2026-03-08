3d033742e26e41b7447b57a5703b5080e1e1a561
David Campbell <noreply@hnefatafl.org>
2026-03-07 19:01:09 -0500

    Double the range of volumes.

--- src/bin/hnefatafl-client/volume.rs
+++ src/bin/hnefatafl-client/volume.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-pub const MAX_VOLUME: u32 = 4;
+pub const MAX_VOLUME: u32 = 8;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub(crate) struct Volume(pub u32);
 
 impl Default for Volume {
     fn default() -> Self {
-        Self(2)
+        Self(4)
     }
 }
 
@@ -15,9 +15,13 @@ impl Volume {
     pub fn volume(&self) -> f32 {
         match self.0 {
             0 => 0.25,
-            1 => 0.5,
-            2 => 1.0,
-            3 => 2.0,
+            1 => 0.345,
+            2 => 0.5,
+            3 => 0.75,
+            4 => 1.0,
+            5 => 1.5,
+            6 => 2.0,
+            7 => 3.0,
             MAX_VOLUME => 4.0,
             _ => unreachable!(),
         }
