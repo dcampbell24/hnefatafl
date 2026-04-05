23894ec449c71bf21aa57cca487b4d95d999f9bf
David Campbell <noreply@hnefatafl.org>
2026-04-05 13:47:20 -0400

    Remove unecessary comment

--- src/bin/hnefatafl-server-full/unix_timestamp.rs
+++ src/bin/hnefatafl-server-full/unix_timestamp.rs
@@ -19,7 +19,6 @@
 use jiff::Timestamp;
 use serde::{Deserialize, Serialize};
 
-/// Non-leap seconds since January 1, 1970 0:00:00 UTC.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub(crate) struct UnixTimestamp(pub Timestamp);
 
