b0b49a1a2f6d79e89b111f7e887b1ddc192aeccc
David Campbell <noreply@hnefatafl.org>
2026-04-17 20:48:57 -0400

    Add a comment.

--- packages/fedora/build.sh
+++ packages/fedora/build.sh
@@ -5,3 +5,4 @@ rpmlint --verbose hnefatafl-copenhagen.spec ../RPMS/x86_64/hnefatafl-copenhagen-
 
 # copr-cli build hnefatafl-copenhagen ~/rpmbuild/SRPMS/hnefatafl-copenhagen-5.6.1-1.fc43.src.rpm
 # sudo dnf install google-noto-fonts-all.noarch R-fontBitstreamVera.noarch
+# sudo dnf upgrade --refresh
