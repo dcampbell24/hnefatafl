be541b02997998a1daa9e83a3499fa418284c084
David Campbell <noreply@hnefatafl.org>
2026-04-20 20:51:25 -0400

    Fix the CHANGELOG.md file.

--- packages/debian/generate-release.sh
+++ packages/debian/generate-release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-PACKAGE='hnefatafl-copenhagen_5.6.1-1_amd64.deb'
+PACKAGE='hnefatafl-copenhagen_5.7.0-1_amd64.deb'
 
 packages/debian/deb.sh
 
@@ -20,7 +20,7 @@ Origin: Hnefatafl Org
 Label: Hnefatafl Copenhagen
 Suite: stable
 Codename: stable
-Version: 5.6.1-1
+Version: 5.7.0-1
 Architectures: amd64
 Components: main
 Description: Copenhagen Hnefatafl client, engine, server and artificial intelligence
