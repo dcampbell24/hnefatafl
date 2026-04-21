0a163b83fa7a55d166ca10ab1856f481e261806e
David Campbell <noreply@hnefatafl.org>
2026-04-20 21:05:26 -0400

    Update the fedora package.

--- packages/fedora/build.sh
+++ packages/fedora/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RUST_MIN_STACK=268435456 rpmbuild -ba hnefatafl-copenhagen.spec
-rpmlint --verbose hnefatafl-copenhagen.spec ../RPMS/x86_64/hnefatafl-copenhagen-5.6.1-4.fc43.x86_64.rpm 
+rpmlint --verbose hnefatafl-copenhagen.spec ../RPMS/x86_64/hnefatafl-copenhagen-5.7.0-1.fc43.x86_64.rpm 
 
 # copr-cli build hnefatafl-copenhagen ~/rpmbuild/SRPMS/hnefatafl-copenhagen-5.6.1-1.fc43.src.rpm
 # sudo dnf install google-noto-fonts-all.noarch R-fontBitstreamVera.noarch
