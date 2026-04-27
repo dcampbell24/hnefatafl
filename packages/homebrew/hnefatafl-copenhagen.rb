677eca0a0dade8c2caded2151963d47cc04777e8
David Campbell <noreply@hnefatafl.org>
2026-04-27 16:30:32 -0400

    Update homebrew.

--- packages/homebrew/hnefatafl-copenhagen.rb
+++ packages/homebrew/hnefatafl-copenhagen.rb
@@ -1,11 +1,13 @@
 cask "hnefatafl-copenhagen" do
-  version "5.8.0"
-  url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
-  sha256 "05bfc820cdb8821a77728b5641699174b53a86796b5e9600e2954766bf9cced8"
   name "hnefatafl-copenhagen"
+  version "5.8.0"
   desc "Client that connects to a server"
   homepage "https://hnefatafl.org"
-  # manpage "hnefatafl-client.1.gz"
+  url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
+  sha256 "05bfc820cdb8821a77728b5641699174b53a86796b5e9600e2954766bf9cced8"
+  license "AGPL-3.0-or-later"
 
-  app "hnefatafl-copenhagen.app"
+  test do
+    system "true"
+  end
 end
