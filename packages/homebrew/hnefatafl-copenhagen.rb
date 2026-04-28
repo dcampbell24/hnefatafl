7b15f3e45a037b45c19633a53b81acdffa035965
David Campbell <noreply@hnefatafl.org>
2026-04-27 22:00:46 -0400

    Order the fields correctly in the cask.

--- packages/homebrew/hnefatafl-copenhagen.rb
+++ packages/homebrew/hnefatafl-copenhagen.rb
@@ -1,14 +1,13 @@
 cask "hnefatafl-copenhagen" do
-  name "hnefatafl-copenhagen"
   version "5.8.0"
-  desc "Client that connects to a server"
-  homepage "https://hnefatafl.org"
-  url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
   sha256 :no_check
-  license "AGPL-3.0-or-later"
+
+  url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
+  name "Hnefatafl Copenhagen"
+  desc "Client that connects to a server"
+  homepage "https://hnefatafl.org/"
+
   depends_on macos: ">= :big_sur"
 
-  test do
-    system "true"
-  end
+  app "hnefatafl-copenhagen.app"
 end
