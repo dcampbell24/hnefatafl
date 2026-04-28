0b83456baa077c30fb1fc2c01196e36135f78170
David Campbell <noreply@hnefatafl.org>
2026-04-27 21:52:31 -0400

    Disable SHA and add requirement of big sur.

--- packages/homebrew/hnefatafl-copenhagen.rb
+++ packages/homebrew/hnefatafl-copenhagen.rb
@@ -4,8 +4,9 @@ cask "hnefatafl-copenhagen" do
   desc "Client that connects to a server"
   homepage "https://hnefatafl.org"
   url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
-  sha256 "92f8775e3701410c8e09d55b0d62d23256109a74ac9c67466bdffdf1dbe8f1a8"
+  sha256 :no_check
   license "AGPL-3.0-or-later"
+  depends_on macos: ">= :big_sur"
 
   test do
     system "true"
