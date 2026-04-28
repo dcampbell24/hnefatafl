61db94cd877baea52afa8e4c19c866f9cf31d848
David Campbell <noreply@hnefatafl.org>
2026-04-27 20:12:55 -0400

    Use shell scripting!

--- packages/homebrew/homebrew.sh
+++ packages/homebrew/homebrew.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -e
 
 INSERT="\
-<key>CFBundleIconFile<\/key>
-<string>shortcut.icns<\/string>
-<\/dict>"
+  <key>CFBundleIconFile</key>
+  <string>shortcut.icns</string>
+</dict>"
 
 mkdir shortcut.iconset
 sips -z 16 16     helmet_1024.png --out shortcut.iconset/icon_16x16.png
@@ -27,7 +27,17 @@ mkdir target/release/bundle/osx/hnefatafl-copenhagen.app/Contents/Resources
 mv packages/homebrew/shortcut.icns target/release/bundle/osx/hnefatafl-copenhagen.app/Contents/Resources
 
 cd target/release/bundle/osx/
-rg --multiline --replace "$INSERT" '<\/dict>' hnefatafl-copenhagen.app/Contents/Info.plist
+
+cd hnefatafl-copenhagen.app/Contents
+while read -r line; do
+    if [[ $line == '</dict>' ]]; then
+        echo "$INSERT"
+    else
+        echo "$line"
+    fi
+done < Info.plist > Info.plist
+cd ../..
+
 tar -czvf hnefatafl-copenhagen.tar.gz hnefatafl-copenhagen.app
 sha256sum hnefatafl-copenhagen.tar.gz
 scp  hnefatafl-copenhagen.tar.gz david@192.168.1.141:~
