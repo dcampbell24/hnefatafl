0d55880b74667e0c9c955163dd6d3ea9b9d1de3b
David Campbell <noreply@hnefatafl.org>
2026-05-02 16:15:59 -0400

    Increase the stack size.

--- packages/fedora/build.sh
+++ packages/fedora/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUST_MIN_STACK=268435456 rpmbuild -ba hnefatafl-copenhagen.spec
+RUST_MIN_STACK=536870912 rpmbuild -ba hnefatafl-copenhagen.spec
 rpmlint --verbose hnefatafl-copenhagen.spec ../RPMS/x86_64/hnefatafl-copenhagen-5.9.0-1.fc44.x86_64.rpm 
 
 # copr-cli build hnefatafl-copenhagen ~/rpmbuild/SRPMS/hnefatafl-copenhagen-5.9.0-1.fc43.src.rpm
