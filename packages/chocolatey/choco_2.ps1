6ae8522c68efabe452b52f4d346a1b2e800a3dcb
David Campbell <noreply@hnefatafl.org>
2026-05-03 16:26:45 -0400

    Do stuff...

--- packages/chocolatey/choco_2.ps1
+++ packages/chocolatey/choco_2.ps1
@@ -1,7 +1,6 @@
 cd hnefatafl-copenhagen/
 
 scp .\tools\hnefatafl-client-installer-*.exe root@hnefatafl.org:~/www/
-scp ..\..\..\target\release\hnefatafl-client.exe david@192.168.1.141:~
 
 choco pack
 choco push --source https://push.chocolatey.org/
