b5ad181d4d63f8c7377f888b213d707f2e536a34
David Campbell <noreply@hnefatafl.org>
2026-05-03 16:26:45 -0400

    Do stuff...

--- packages/windows-steam.ps1
+++ packages/windows-steam.ps1
@@ -1,2 +1,3 @@
 sed -i 's/cargo-/windows-steam-/' src\bin\hnefatafl-client\main.rs;
 cargo build --release --bin hnefatafl-client --features client --no-default-features
+scp ..\..\..\target\release\hnefatafl-client.exe david@192.168.1.141:~
