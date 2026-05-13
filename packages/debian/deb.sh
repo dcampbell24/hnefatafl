38768dc10c0f9a0855167fe78252ad0bb26eae38
David Campbell <noreply@hnefatafl.org>
2026-05-13 18:51:15 -0400

    Replace AI with taflzero.

--- packages/debian/deb.sh
+++ packages/debian/deb.sh
@@ -3,15 +3,15 @@
 export RUST_MIN_STACK=67108864
 
 sed -i 's/cargo-/debian-/' src/bin/hnefatafl-client/main.rs;
-cargo build --release
+cargo build --release --examples
 
-./target/release/hnefatafl-ai --man --username ""
+./target/release/examples/taflzero --man --username ""
 ./target/release/hnefatafl-client --man
 ./target/release/hnefatafl-server --man
 ./target/release/hnefatafl-server-full --man
 ./target/release/hnefatafl-text-protocol --man
 
-gzip --no-name --best hnefatafl-ai.1
+gzip --no-name --best taflzero.1
 gzip --no-name --best hnefatafl-server.1
 gzip --no-name --best hnefatafl-text-protocol.1
 gzip --no-name --best hnefatafl-client.1
@@ -19,7 +19,7 @@ gzip --no-name --best hnefatafl-server-full.1
 
 PACKAGE=$(cargo deb)
 
-rm hnefatafl-ai.1.gz
+rm taflzero.1.gz
 rm hnefatafl-server.1.gz
 rm hnefatafl-text-protocol.1.gz
 rm hnefatafl-client.1.gz
