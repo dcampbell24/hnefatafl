a1101fa824dbb07d4a710193a58419053b2adcbb
David Campbell <noreply@hnefatafl.org>
2026-05-17 00:49:40 -0400

    ...

--- src/tests.rs
+++ src/tests.rs
@@ -2385,6 +2385,30 @@ fn can_not_escape_2() -> anyhow::Result<()> {
     assert_eq!(game.status, Status::AttackerWins);
 
     let board: Board = [
+        "..XXXX..X..",
+        ".X.......X.",
+        "X.........X",
+        "........OOO",
+        "X......O.KO",
+        "X......OOOX",
+        "..........X",
+        "X.........X",
+        "X.........X",
+        ".X.......X.",
+        "..XXXX..X..",
+    ]
+    .try_into()?;
+
+    let mut game = game::Game {
+        board,
+        ..Default::default()
+    };
+
+    game.read_line("play attacker f1 h1")?;
+    assert!(!game.board.can_not_esacpe());
+    assert_eq!(game.status, Status::Ongoing);
+
+    let board: Board = [
         ".....X..X..",
         "....X....X.",
         "...X......X",
