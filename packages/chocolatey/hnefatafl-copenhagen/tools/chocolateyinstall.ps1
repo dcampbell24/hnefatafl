420635b06f368e0aed3260c185ab3c2ec7643789
David Campbell <dcampbell24@gmail.com>
2026-07-12 18:41:01 -0400

    Update the choco package.

--- packages/chocolatey/hnefatafl-copenhagen/tools/chocolateyinstall.ps1
+++ packages/chocolatey/hnefatafl-copenhagen/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'hnefatafl-client-installer-6.0.1.exe'
+$fileLocation = Join-Path $toolsDir 'hnefatafl-client-installer-6.1.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
