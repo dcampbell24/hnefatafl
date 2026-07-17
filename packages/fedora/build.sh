d6a1d65009abaf661dfe8e4898ad60d6a574f72c
David Campbell <noreply@hnefatafl.org>
2026-07-17 18:01:09 -0400

    Update the fedora package.

--- packages/fedora/build.sh
+++ packages/fedora/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rpmbuild -ba hnefatafl-copenhagen.spec
-rpmlint --verbose hnefatafl-copenhagen.spec ../RPMS/x86_64/hnefatafl-copenhagen-6.2.1-1.fc44.x86_64.rpm
-copr-cli build hnefatafl-copenhagen ~/rpmbuild/SRPMS/hnefatafl-copenhagen-6.2.1-1.fc44.src.rpm
+rpmlint --verbose hnefatafl-copenhagen.spec ../RPMS/x86_64/hnefatafl-copenhagen-6.2.2-1.fc44.x86_64.rpm
+copr-cli build hnefatafl-copenhagen ~/rpmbuild/SRPMS/hnefatafl-copenhagen-6.2.2-1.fc44.src.rpm
 
 # sudo dnf upgrade --refresh
