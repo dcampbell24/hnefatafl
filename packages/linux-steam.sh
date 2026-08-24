704c099daf3c0b35bc7b986cd8446b02129f8011
David Campbell <noreply@hnefatafl.org>
2026-08-24 14:47:43 -0400

    Update steam-linux.

--- packages/linux-steam.sh
+++ packages/linux-steam.sh
@@ -4,3 +4,4 @@ export RUST_MIN_STACK=67108864
 
 sed -i 's/cargo-/linux-steam-/' src/lib.rs;
 cargo build --release --bin hnefatafl-client --features client --no-default-features
+sed -i 's/linux-steam-/cargo-/' src/lib.rs;
